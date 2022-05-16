26e7100039b164deee3061630f35f415a28e0cba
rakib-hasan <mahmud1807077@stud.kuet.ac.bd>
2022-05-17 00:33:54 +0600

    practice commit1

---
+++ app/src/main/java/com/android/iunoob/bloodbank/activities/practice.java
@@ -0,0 +1,17 @@
+package com.android.iunoob.bloodbank.activities;
+
+public class practice {
+    int a;
+
+    public practice(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
